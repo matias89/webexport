a1102ba70a85301d05f025bcf4adbc3dfdc4e920
Matias Aybar <matiasaybar89@gmail.com>
2019-08-09 19:42:15 -0300

    Formik agregado

--- src/pages/ContactView/ContactView.js
+++ src/pages/ContactView/ContactView.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
+import { Formik } from 'formik';
 
 class ContactView extends Component {
     constructor(props) {
         super(props);
-        this.handleOnChange = this.handleOnChange.bind(this);
-        this.state = {
-            value: ''
-        }
-    }
-    handleOnChange(event) {
-        this.setState({
-            value: event.target.value
-        });
     }
     render() {
         return (
             <div>
                 <h2>Contact</h2>
-                <input
-                    type="text"
-                    value={this.state.value}
-                    className="form-control mt-4 mb-4"
-                    onChange={this.handleOnChange}
-                />
+                <Formik
+                    initialValues={{
+                        email: 'matiasaybar89@gmail.com',
+                        user: 'matias'
+                    }}
+                    validate={values => {
+                        console.log('>>> validate', values);
+                        let errors = {};
+                        if(values.user === '') {
+                            errors.user = 'Required';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values) => {
+                        console.log('>>> Values', values);
+                    }}
+                >
+                    {({
+                        values,
+                        handleSubmit,
+                        handleChange,
+                        errors
+                    }) => {
+                        return (
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    type="email"
+                                    className="form-control mt-4 mb-4"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    name="email"
+                                />
+                                <input
+                                    type="text"
+                                    className="form-control mt-4 mb-4"
+                                    value={values.user}
+                                    onChange={handleChange}
+                                    name="user"
+                                />
+                                {errors.user ? errors.user : null}
+                                <button type="submit">Enviar</button>
+                            </form>
+                        );
+                    }}
+                </Formik>
             </div>
         );
     };
